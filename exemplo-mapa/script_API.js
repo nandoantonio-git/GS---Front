5432c76eb577314e0b431b77c206dd61eaa819b7
Thomas <thomasalmeidar@gmail.com>
2024-06-07 23:54:18 -0300

    carousel

--- exemplo-mapa/script_API.js
+++ exemplo-mapa/script_API.js
@@ -45,3 +45,23 @@ function validateInput(input) {
 		input.value = input.value.replace(/[0-9]/g, '');
 	}
 }
+
+let currentIndex = 0;
+
+function showImage(index) {
+	const images = document.querySelector('.carousel-images');
+	const imageWidth = images.clientWidth;
+	images.style.transform = 'translateX(' + -index * imageWidth + 'px)';
+}
+
+function prevImage() {
+	const images = document.querySelectorAll('.carousel-images img');
+	currentIndex = currentIndex > 0 ? currentIndex - 1 : images.length - 1;
+	showImage(currentIndex);
+}
+
+function nextImage() {
+	const images = document.querySelectorAll('.carousel-images img');
+	currentIndex = currentIndex < images.length - 1 ? currentIndex + 1 : 0;
+	showImage(currentIndex);
+}
