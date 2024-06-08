88b8e45970a6d2214659ddb1ad008e1ca7b3acea
Thomas <thomasalmeidar@gmail.com>
2024-06-07 23:43:31 -0300

    web

---
+++ exemplo-mapa/script_API.js
@@ -0,0 +1,32 @@
+function expandSidebar() {
+	const sidebar = document.getElementById('secundario');
+	const principal = document.getElementById('principal');
+
+	if (sidebar.classList.contains('expanded')) {
+		sidebar.classList.remove('expanded');
+		principal.classList.remove('expanded-lateral');
+	} else {
+		sidebar.classList.add('expanded');
+		principal.classList.add('expanded-lateral');
+	}
+}
+
+function expandBottomBar() {
+	const bottomBar = document.getElementById('terciario');
+	const principal = document.getElementById('principal');
+
+	if (bottomBar.classList.contains('expanded')) {
+		bottomBar.classList.remove('expanded');
+		principal.classList.remove('expanded-inferior');
+	} else {
+		bottomBar.classList.add('expanded');
+		principal.classList.add('expanded-inferior');
+	}
+}
+
+var map = L.map('map').setView([51.505, -90], 5);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 15,
+	attribution:
+		'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
